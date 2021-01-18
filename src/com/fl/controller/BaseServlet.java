7f25e433b505b56308fa3b0a2d82392ed4ce89a3
fl1906 <1906249647@qq.com>
2021-01-18 20:47:32 +0800

    购物车项目

--- src/com/fl/controller/BaseServlet.java
+++ src/com/fl/controller/BaseServlet.java
@@ -47,6 +47,9 @@ public class BaseServlet extends HttpServlet {
             }
         } catch (Exception e) {
             e.printStackTrace();
+           //异常统一处理的地方
+            req.setAttribute("msg","程序异常了哟~~");
+            resp.sendRedirect("/msg.jsp");
         }
     }
     public String index(HttpServletRequest req,HttpServletResponse resp){
